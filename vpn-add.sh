a2fabe443db30379c1f38284ae861090843c496f
jm33-m0 <jm33.m0@gmail.com>
2016-10-06 12:24:36 +0800

    vpn-add

--- vpn-add.sh
+++ vpn-add.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo -e 'Username: '
+echo -n 'Username: '
 read user
-name=$(echo $user | base64)
-file=$(echo $name | md5sum | cut -d '-' -f1)
+export name=$(echo $user | base64)
+export file=$(echo $name | md5sum | cut -d '-' -f1)
 cat << EOF > user.tmpl
 cn = "lwk"
 unit = "vpn"
@@ -14,3 +14,4 @@ certtool --generate-privkey --outfile user-key.pem
 certtool --generate-certificate --load-privkey user-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template user.tmpl --outfile user-cert.pem
 certtool --to-p12 --load-privkey user-key.pem --pkcs-cipher 3des-pkcs12 --load-certificate user-cert.pem --outfile user.p12 --outder
 cp ./user.p12 /etc/ocserv/$user$file.p12
+rm user*
